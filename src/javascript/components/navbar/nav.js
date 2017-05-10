5201a5151a9fd3a7f67b58c1495c787de277e46b
K.C. Jones-Evans <kc.jones-evans@appliedis.com>
2017-05-10 12:39:12 -0400

    removed formatting changed app to javascript

---
+++ src/javascript/components/navbar/nav.js
@@ -0,0 +1,32 @@
+$(window).resize(function() {
+  let more = document.getElementById('js-navigation-more');
+  if ($(more).length > 0) {
+    let windowWidth = $(window).width();
+    let moreLeftSideToPageLeftSide = $(more).offset().left;
+    let moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+    if (moreLeftSideToPageRightSide < 330) {
+      $('#js-navigation-more .submenu .submenu').removeClass('fly-out-right');
+      $('#js-navigation-more .submenu .submenu').addClass('fly-out-left');
+    }
+
+    if (moreLeftSideToPageRightSide > 330) {
+      $('#js-navigation-more .submenu .submenu').removeClass('fly-out-left');
+      $('#js-navigation-more .submenu .submenu').addClass('fly-out-right');
+    }
+  }
+});
+
+$(document).ready(function() {
+  let menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass('show');
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
